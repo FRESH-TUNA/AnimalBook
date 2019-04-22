d915daf0ba1ad2beaeef77645f4f5a9464181b77
DONGWON KIM <lunacircle4@gmail.com>
2019-04-22 18:31:23 +0900

    setting 파일 수정

--- EasyDjangoRestProject/settings.py
+++ EasyDjangoRestProject/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(nk*8(hrv!c3ri4g)1ecoi&&bj7l5ilidor1*$rlfzzf=9$arc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
